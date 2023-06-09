6c1a2418055acae9e2faac0031b6597512252832
Math Costa <xcudder@hotmail.com>
2023-06-09 10:35:44 -0300

    Finish commune

--- client/sleep_command.lua
+++ client/sleep_command.lua
@@ -4,6 +4,8 @@ local xPlayer = false
 local hasBlanket = false
 local restQuality = 1
 local isInPaletoComune = false
+local ov3 = vector3(-102.1, 6345.04, 31.58)
+local iv3 = vector3(151.41, -1007.96, -99.0)
 
 -- Hooks and callbacks
 RegisterNetEvent('esx_needtosleep:enteredProperty')
@@ -23,33 +25,68 @@ end)
 
 -- Keep commune running
 CreateThread(function()
-	local v3 = vector3(-102.1, 6345.04, 31.58)
-	local lv3 = vector3(151.41, -1007.96, -99.0)
-	setupBlip({title="Paleto Commune", colour=1, id=492 }, v3)
+	setupBlip({title="Paleto Commune", colour=1, id=492 }, ov3)
 	while true do
-		Wait(0)
-		DrawMarker(0, v3.x, v3.y, v3.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.0, 1.0, 1.0, 244, 244, 23, 100, true, true, 2, true, false, false, false)	
-		DrawMarker(0, lv3.x, lv3.y, lv3.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.0, 1.0, 1.0, 244, 244, 23, 100, true, true, 2, true, false, false, false)	
-		
-		-- if coordinates_close_enough(v3) then
-		-- 	DisplayHelpText("Press ~INPUT_CONTEXT~ to enter the ~y~commune")
-		-- 	if(IsControlJustReleased(1, 38)) then
-		-- 		SetEntityCoords(PlayerPedId(), lv3.x, lv3.y, lv3.z)
-		-- 		isInPaletoComune = true
-		-- 	end
-		-- end
+		Citizen.Wait(0)
+		DrawMarker(0, ov3.x, ov3.y, ov3.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.0, 1.0, 1.0, 244, 244, 23, 100, true, true, 2, true, false, false, false)
+		DrawMarker(0, iv3.x, iv3.y, iv3.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.0, 1.0, 1.0, 244, 244, 23, 100, true, true, 2, true, false, false, false)
 
-		-- if coordinates_close_enough(lv3) then
-		-- 	DisplayHelpText("Press ~INPUT_CONTEXT~ to exit the ~y~commune")
-		-- 	if(IsControlJustReleased(1, 38)) then
-		-- 		SetEntityCoords(PlayerPedId(), v3.x, v3.y, v3.z)
-		-- 		isInPaletoComune = false
-		-- 	end
-		-- end
+		if coordinates_close_enough(ov3) then
+		DisplayHelpText("Press ~INPUT_CONTEXT~ to enter the ~y~commune")
+			if(IsControlJustReleased(1, 38)) then
+				DoScreenFadeOut(1000)
+				Wait(1000)
+				SetEntityCoords(PlayerPedId(), 151.41, -1007.96, -100.0)
+				DoScreenFadeIn(1000)
+				createCommunePeds()
+				isInPaletoComune = true
+			end
+		end
+
+		if coordinates_close_enough(iv3) then
+			DisplayHelpText("Press ~INPUT_CONTEXT~ to exit the ~y~commune")
+			if(IsControlJustReleased(1, 38)) then
+				DoScreenFadeOut(1000)
+				Wait(1000)
+				SetEntityCoords(PlayerPedId(), -102.1, 6345.04, 30.58)
+				DoScreenFadeIn(1000)
+				isInPaletoComune = false
+			end
+		end
 	end
 end)
 
 -- Silly logic abstractions
+function createCommunePeds()
+	ClearAreaOfPeds(154.17, -1002.8, -100.0, 5)
+	createSingleCommunePed(0x4705974A, vector3(154.90, -1002.50, -100.50), "PROP_HUMAN_SEAT_CHAIR", 110.0)
+	createSingleCommunePed(0x6A8F1F9B, vector3(152.36, -1000.14, -100.0), "WORLD_HUMAN_MUSCLE_FLEX", 0.0)
+	createSingleCommunePed(0x48F96F5B, vector3(154.36, -1004.50, -99.42), "WORLD_HUMAN_BUM_SLUMPED", 80.0)
+	createSingleCommunePed(0x1EC93FD0, vector3(154.32, -1006.46, -100.0), "WORLD_HUMAN_SMOKING_POT", 80.0)
+	createSingleCommunePed(0x174D4245, vector3(153.56, -1007.63, -100.50), "PROP_HUMAN_SEAT_CHAIR", 0.0)
+	createSingleCommunePed(0x8CA0C266, vector3(150.65, -1006.45, -100.0), "WORLD_HUMAN_LEANING", 240.0)
+	createSingleCommunePed(0x53B57EB0, vector3(153.44, -1005.26, -100.50), "PROP_HUMAN_SEAT_CHAIR", 80.0)
+end
+
+function createSingleCommunePed(hash, v3, ped_scenario, heading)
+	RequestModel(hash)
+
+	while not HasModelLoaded(hash) do
+		Wait(1)
+	end
+
+	local ped = CreatePed(1, hash,v3.x,v3.y,v3.z, 60, false, true)
+	SetBlockingOfNonTemporaryEvents(ped, true)
+	SetPedDiesWhenInjured(ped, false)
+	SetPedCanPlayAmbientAnims(ped, true)
+	SetPedCanRagdollFromPlayerImpact(ped, false)
+	SetEntityInvincible(ped, true)
+	SetEntityHeading(ped, heading)
+	FreezeEntityPosition(ped, true)
+	TaskStartScenarioInPlace(ped, ped_scenario, 0, true);
+	return ped
+end
+
 function sleepCommand(source, args)
 	-- 8 in game hours (16 IRL minutes) should have you all rested, this takes into account
 	-- the esx status tick that would be adding sleep even now... that should probably
@@ -153,4 +190,10 @@ end
 function coordinates_close_enough(arg)
 	local A = GetEntityCoords(PlayerPedId(), false)
 	return Vdist(arg.x, arg.y, arg.z, A.x, A.y, A.z) < 1.5
+end
+
+function DisplayHelpText(str)
+	SetTextComponentFormat("STRING")
+	AddTextComponentString(str)
+	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
 end
