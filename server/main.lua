de9eb32e7e4f8ca18d2b07745bed79fb85dcb913
Math Costa <xcudder@hotmail.com>
2023-06-20 22:58:56 -0300

    Improve usables slightly

--- server/main.lua
+++ server/main.lua
@@ -27,13 +27,13 @@ CreateThread(function()
 			end
 
 			if k == "junkfood" then
-				TriggerClientEvent("esx_status:add", source, "hunger", 80000)
-				TriggerClientEvent("esx_status:add", source, "sleepiness", 100000) -- detrimental add
+				TriggerClientEvent("esx_status:add", source, "hunger", 100000)
+				TriggerClientEvent("esx_status:add", source, "sleepiness", 80000) -- detrimental add
 				TriggerClientEvent("esx_status:remove", source, "stress", 30000) -- benign remove
 				TriggerClientEvent('esx_basicneeds:onUse', source, v.type, v.prop)
 			elseif k == "suggarydrink" then
-				TriggerClientEvent("esx_status:add", source, "thirst", 80000)
-				TriggerClientEvent("esx_status:add", source, "sleepiness", 100000) -- detrimental add
+				TriggerClientEvent("esx_status:add", source, "thirst", 100000)
+				TriggerClientEvent("esx_status:add", source, "sleepiness", 80000) -- detrimental add
 				TriggerClientEvent("esx_status:remove", source, "stress", 30000) -- benign remove
 				TriggerClientEvent('esx_basicneeds:onUse', source, v.type, v.prop)
 			elseif k == "coffee" then
@@ -41,14 +41,12 @@ CreateThread(function()
 				TriggerClientEvent("esx_status:remove", source, "sleepiness", 50000) -- benign remove
 				TriggerClientEvent('esx_basicneeds:onUse', source, v.type, v.prop)
 			elseif k == "whisky" then
-				TriggerClientEvent("esx_status:remove", source, "stress", 100000) -- benign remove
-				TriggerClientEvent("esx_status:add", source, "sleepiness", 5000) -- detrimental add
+				TriggerClientEvent("esx_status:remove", source, "stress", 200000) -- benign remove
+				TriggerClientEvent("esx_status:add", source, "sleepiness", 50000) -- detrimental add
 				TriggerClientEvent('esx_basicneeds:onUse', source, v.type, v.prop)
 			elseif k == "cigarette" then
-				TriggerClientEvent("esx_status:remove", source, "stress", 50000) -- benign remove
+				TriggerClientEvent("esx_status:remove", source, "stress", 200000) -- benign remove
 				TriggerClientEvent("esx_needtosleep:cigaretteUsed", source)
-			else
-				print(string.format('^1[ERROR]^0 %s has no correct type defined.', k))
 			end
 		end)
 	end 
